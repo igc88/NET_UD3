9731d5d59e3597b8dd6bd9f56abe6a477d64cacd
Iago <iago.gc88@gmail.com>
2020-12-23 11:55:10 +0100

    E3

---
+++ U3_E3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace U3_E3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            double a = 2.5;
+            char c = 'a';
+
+            Console.WriteLine("Valor de n: {0}", n);
+            Console.WriteLine("Valor de a: {0}", a);
+            Console.WriteLine("Valor de c: {0}", c);
+
+            Console.WriteLine("Valor de n+a: {0}", n + a);
+            Console.WriteLine("Valor de a-n: {0}", a - n);
+
+            Console.WriteLine("Valor numerico de c: {0}", Char.GetNumericValue(c));
+
+            Console.ReadKey();
+        }
+    }
+}
