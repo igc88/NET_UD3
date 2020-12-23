d7c79fecd45c5ad4bb882fa2b72f22c788379451
Iago <iago.gc88@gmail.com>
2020-12-23 12:03:06 +0100

    E3

---
+++ U3_E5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U3_E5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int c = 4;
+            int d = 5;
+
+            b = c;
+            c = a;
+            a = d;
+            d = b;
+        }
+    }
+}
