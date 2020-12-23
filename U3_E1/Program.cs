f77b18420571f8b5e33bbe9b5c85e15212a5f8e5
Iago <iago.gc88@gmail.com>
2020-12-23 11:46:58 +0100

    E1

---
+++ U3_E1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace U3_E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine("Suma: {0}", a + b);
+            Console.WriteLine("Resta: {0}", a - b);
+            Console.WriteLine("Multuplicación: {0}", a * b);
+            Console.WriteLine("División: {0}", a / b);
+            Console.WriteLine("Módulo: {0}", a % b);
+
+            Console.ReadKey();
+        }
+    }
+}
